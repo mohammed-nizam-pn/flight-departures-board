3165c8d4d4e421e1c7e3e72cd4b36e08200ac978
NizamMuhammed <mohammednizampn@gmail.com>
2024-03-25 15:34:05 +0000

    started with axios fetch api test

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/no-babel'
+  preset: "@vue/cli-plugin-unit-jest/presets/no-babel",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "^axios$": "axios/dist/node/axios.cjs",
+  },
 }
---
+++ tests/unit/services/FlightDataService.spec.js
@@ -0,0 +1,2 @@
+import axios from "axios"
+import { fetchFlightData } from "@/services/FlightDataService.js"
